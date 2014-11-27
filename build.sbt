1ad1abc3bbd9859575679be247083d9ffcf9daaa
Michał Zając <michal.t.zajac@gmail.com>
2014-11-27 21:07:00 +0100

    ThreeSum series runner

--- build.sbt
+++ build.sbt
@@ -5,5 +5,7 @@ version := "0.1"
 scalaVersion := "2.11.4"
 
 libraryDependencies ++= Seq(
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
+  "ch.qos.logback" % "logback-classic" % "1.1.2",
   "org.scalatest" %% "scalatest" % "2.2.1" % "test"
 )
--- src/main/scala/michalz/algo/analysis/ThreeSum.scala
+++ src/main/scala/michalz/algo/analysis/ThreeSum.scala
@@ -1,6 +1,7 @@
 package michalz.algo.analysis
 
-import michalz.algo.utils.{StopWatch, FileUtils}
+import com.typesafe.scalalogging.LazyLogging
+import michalz.algo.utils.{FileUtils, StdRandom, StopWatch}
 
 /**
  * @author mzajac
@@ -21,12 +22,54 @@ object ThreeSum {
   }
 }
 
-object TreeSumFileRunner {
+object ThreeSumFileRunner {
 
   def main(args: Array[String]) {
     val input: Array[Int] = FileUtils.readIntsFromFile(args(0))
     val stopWatch: StopWatch = StopWatch.start
     val numberOfTriples: Int = ThreeSum.calculate(input)
     println("Completed in %s, found %d triples".format(stopWatch.elapsedTime, numberOfTriples))
+  }
+}
+
+
+object ThreeSumRandomRunner {
+  val fromRange: Int = -10000
+  val toRange: Int = 10000
+
+  def main(args: Array[String]) {
+    val numberOfInts: Int = args(0).toInt
+    val stdRandom: StdRandom = new StdRandom
+    val generatedArray: Array[Int] = stdRandom.intRange(numberOfInts, fromRange, toRange)
+    val stopWatch: StopWatch = StopWatch.start
+    val nrOfTriples: Int = ThreeSum.calculate(generatedArray)
+    println("Completed in %s, found %d triples".format(stopWatch.elapsedTime, nrOfTriples))
+  }
+}
+
+object ThreeSumSeriesRunner extends LazyLogging {
+
+  val stdRandom = new StdRandom
+  val fromRange = -100000
+  val toRange = 100000
+
+  private def performCalculation(size: Int) = {
+    val generatedInput: Array[Int] = stdRandom.intRange(size, fromRange, toRange)
+    val stopWatch = StopWatch.start
+    ThreeSum.calculate(generatedInput)
+    stopWatch.elapsedTime
+  }
+
+  def main(args: Array[String]) {
+    val maxNumber = args(0).toInt
+    val start = 1000
+    performR(start)
+
+    def performR(size: Int): Unit = {
+      if(size <= maxNumber){
+        logger.info("Size %d, time %s".format(size, performCalculation(size)))
+        performR(size * 2)
+      }
+    }
   }
 }
